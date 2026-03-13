b949fe3bea224a82c4ef0094440bfa9cdb0ca873
AdmiAgus <admcall4.cc@gmail.com>
2026-03-12 23:45:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get(/^\/promotores\/.*$/, (req, res) => {
 // });
 
 // Escuchar en el puerto 80
-const PORT = 7595;
+const PORT = 80;
 app.listen(PORT, () => {
     console.log(`\n🚀 Servidor corriendo en el puerto ${PORT}`);
     console.log(`✅ App de promotores: http://localhost/promotores`);
